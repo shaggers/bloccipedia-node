57bcff872208715af2b67b1b4580bacca1b4959d
shaggers <camtl66@gmail.com>
2019-03-27 15:13:44 -0500

    index page is setup and styled

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -3,15 +3,15 @@ const server = require("../../src/server");
 const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
-  
+
 
   describe("GET /", () => {
 
-    it("should return status code 200", (done) => {
+    it("should return status code 200 and have bloccipedia in the body of the response", (done) => {
 
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-
+        expect(body).toContain("Bloccipedia");
         done();
       });
     });
